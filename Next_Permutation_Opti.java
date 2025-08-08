aca4da7cbc27cd1c4b907e9b11e094e1d239df04
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-08 14:29:58 +0530

    8/8/25

--- Next_Permutation_Opti.java
+++ Next_Permutation_Opti.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 
 public class Next_Permutation_Opti {
@@ -30,7 +29,8 @@ public class Next_Permutation_Opti {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+        //int[] arr = {1, 2, 3};
+        int[] arr={2,3,5,4,1,0,0};
         nextPermutation(arr);
         System.out.println("Next Permutation: " + Arrays.toString(arr));
     }
