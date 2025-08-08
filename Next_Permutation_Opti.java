370b22016eef8adcc14e65998d871549388a0d86
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-08 14:17:12 +0530

    8/8/25

---
+++ Next_Permutation_Opti.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+
+public class Next_Permutation_Opti {
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        nextPermutation(arr);
+        System.out.println("Next Permutation: " + Arrays.toString(arr));
+    }
+}
