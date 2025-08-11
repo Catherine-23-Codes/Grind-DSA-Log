74f339315c94dba1a85417c29422b3c3050c6ddd
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-11 10:54:33 +0530

    11/8/25

---
+++ LongestSubarray1.java
@@ -0,0 +1,26 @@
+public class LongestSubarray1 {
+
+    public static int longestSubarrayWithSumK(int[] arr, int k) {
+        int n = arr.length;
+        int maxLen = 0;
+
+        // Check all subarrays
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 2, 7, -1, 0, 6, 1};
+        int k = 8;
+        int length = longestSubarrayWithSumK(arr, k);
+        System.out.println("Longest subarray length with sum " + k + " is: " + length);
+    }
+}
