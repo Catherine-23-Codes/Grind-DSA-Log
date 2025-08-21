fea12c8fbfeef76ec686570a1aedf4ea247d97c1
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-21 08:03:29 +0530

    21/8/25

---
+++ countSubarraysK2.java
@@ -0,0 +1,25 @@
+public class countSubarraysK2 {
+
+    public static int countSubarraysBetter(int[] arr, int k) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
+        int k = 3;
+        System.out.println(countSubarraysBetter(arr, k));  // Output: 6
+    }
+}
+
+
