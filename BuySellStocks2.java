3bb93fba4ffd8d2d38007f09776f78afaa352ec9
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-03 07:36:58 +0530

    3/9/25

---
+++ BuySellStocks2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BuySellStocks2 {
+    public static int buyAndSellStocks(int prices[]) {
+    int buyPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+        if (buyPrice < prices[i]) {   // can sell today
+            int profit = prices[i] - buyPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        } else {
+            buyPrice = prices[i];     // update buy price if smaller
+        }
+    }
+    return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] prices={7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum Profit: " + buyAndSellStocks(prices));
+    }
+}
