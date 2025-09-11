742e76b87b2ecd413213cdf7602ee0d782980081
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-11 06:46:13 +0530

    11/9/25

--- Spiral_Order_Opti.java
+++ Spiral_Order_Opti.java
@@ -45,14 +45,14 @@ public class Spiral_Order_Opti{
     }
 
     public static void main(String[] args) {
-        int[][] mat = {
+        int[][] matrix = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
             {9, 10, 11, 12},
             {13, 14, 15, 16}
         };
         
-        System.out.println(spiralOrder(mat));
+        System.out.println(spiralOrder(matrix));
     }
 }
 
