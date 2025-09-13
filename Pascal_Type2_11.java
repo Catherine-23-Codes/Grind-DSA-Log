06847c3589c5569cbe48b2dfe6bc974e1d6e035b
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-13 09:41:02 +0530

    13/9/25

---
+++ Pascal_Type2_11.java
@@ -0,0 +1,19 @@
+public class Pascal_Type2_11 {
+    static void printNCR(int n) {
+        long ans = 1;
+        System.out.print(ans + " "); // first element of row is always 1
+
+        for (int col = 1; col <= n; col++) {
+            ans = ans * (n - col + 1);  // multiply by (n - col + 1)
+            ans = ans / col;            // divide by col
+            System.out.print(ans + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print("Row " + n + ": ");
+        printNCR(n);
+    }
+}
+
