0a7fcf5a97edab2af6266d56ac96678c4139bdce
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-14 08:39:45 +0530

    14/9

---
+++ Strings/Strings.java
@@ -0,0 +1,14 @@
+// Strings are Immutable- which means they can't be changed! 
+import java.util.*;
+public class Strings{
+    public static void main(String[] args) {
+    // assigning char & str values 
+        char arr[]={'a','b','c','d'};
+
+        //Initialising and declaring string 
+        String str1="Catherine";
+        String str2="Vinaya";
+        System.out.println("String str1:"+str1);
+        System.out.println("String str2:"+str2);
+    }
+}
