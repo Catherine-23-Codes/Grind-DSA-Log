c2e94c7d3bb59b3bb6102d7456cf0486c658e1cc
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-15 06:55:09 +0530

    15/9/

---
+++ Strings/Str_Length.java
@@ -0,0 +1,11 @@
+//Length of the string is found using .length()
+import java.util.*;
+public class Str_Length {
+    public static void main(String[] args) {
+        String s1=new String();
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        System.out.println(s1);
+        System.out.println("The length of the string is:"+s1.length());
+    }
+}
