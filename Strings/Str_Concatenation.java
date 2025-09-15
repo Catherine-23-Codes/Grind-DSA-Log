196e5dc0809cb9078fba1113617b7f758c38b952
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-15 07:05:10 +0530

    15/9

---
+++ Strings/Str_Concatenation.java
@@ -0,0 +1,9 @@
+public class Str_Concatenation {
+    public static void main(String[] args) {
+        String firstName="Catherine";
+        String lastName="Infanta";
+        String fullName=firstName+" "+lastName;
+        System.out.println("The full name is:"+fullName);
+        System.out.println("The length of my name is:"+ fullName.length());
+    }
+}
