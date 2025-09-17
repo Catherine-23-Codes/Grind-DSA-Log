6ed7d0833242670146016923eb7ff6d9a07944a5
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-17 10:13:27 +0530

    17/9

---
+++ Strings/p1.java
@@ -0,0 +1,11 @@
+public class p1 {
+    public static void LengthofString(){
+        String str1="Catherine";
+        String str2="Catherine"+ "Infanta";
+        System.out.println("the length of the string is:"+str1.length());
+        System.out.println("the length of the string is:"+str2.length());
+    }
+    public static void main(String args[]){
+        LengthofString();
+    }
+}
