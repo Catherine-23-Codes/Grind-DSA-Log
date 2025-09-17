47a465c716a2baf8c398ddaf79d81ddce88db28a
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-17 07:47:07 +0530

    17/9

---
+++ Strings/charAt1.java
@@ -0,0 +1,9 @@
+public class charAt1 {
+    public static void main(String[] args) {
+        String firstName="Catherine";
+        String lastName="Infanta";
+        String fullName=firstName+lastName;
+        System.out.println("the length of the str is:"+ fullName.length());
+        System.out.println("The full name characters are:"+ fullName.charAt(12));
+    }
+}
