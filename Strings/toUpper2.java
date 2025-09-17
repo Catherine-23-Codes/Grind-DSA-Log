2ed90f605869a8b5d4dfc17545e0085e2db685b9
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-17 10:04:42 +0530

    17/9/25

---
+++ Strings/toUpper2.java
@@ -0,0 +1,9 @@
+public class toUpper2 {
+    public static void ToUpperCase(String str){
+        System.out.println("the string is:"+str.toUpperCase());
+    }
+    public static void main(String[] args) {
+        String str="Catherine" +"Infanta";
+        ToUpperCase(str);
+    }
+}
