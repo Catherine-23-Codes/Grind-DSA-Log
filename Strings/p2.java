6f5fe5e4b732b6a0b7ce58e83081929fb852b605
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-17 10:18:54 +0530

    17/9/25

--- Strings/p2.java
+++ Strings/p2.java
@@ -5,7 +5,9 @@ public class p2 {
         System.out.println("the uppercase of string is:"+ str1.toUpperCase());
         System.out.println("the uppercase of string is:"+ str2.toUpperCase());
         System.out.println("the lower of string is:"+ str1.toLowerCase());
-         System.out.println("the lower of string is:"+ str2.toLowerCase());
+        System.out.println("the lower of string is:"+ str2.toLowerCase());
+        System.out.println("the length of the string is:"+str1.length());
+        System.out.println("the length of the string is:"+str2.length());
     }
     public static void main(String[] args) {
     Example();
