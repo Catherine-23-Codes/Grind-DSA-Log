8580fe70455c0fe79736a4af6d5b6fddf4ac4789
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-17 10:17:54 +0530

    17/9/25

---
+++ Strings/p2.java
@@ -0,0 +1,13 @@
+public class p2 {
+    public static void Example(){
+        String str1="Catherine";
+        String str2="Vinaya";
+        System.out.println("the uppercase of string is:"+ str1.toUpperCase());
+        System.out.println("the uppercase of string is:"+ str2.toUpperCase());
+        System.out.println("the lower of string is:"+ str1.toLowerCase());
+         System.out.println("the lower of string is:"+ str2.toLowerCase());
+    }
+    public static void main(String[] args) {
+    Example();
+    }
+}
