11a50db458df21e6344927c494a3cb5875a83ed2
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-19 21:42:29 +0530

    19/9

---
+++ Strings Leetcode/Palindrome2.java
@@ -0,0 +1,18 @@
+public class Palindrome2 {
+    public static boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));  // true
+        System.out.println(isPalindrome(-121)); // false
+    }
+}
+
