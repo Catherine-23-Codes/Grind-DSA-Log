ef49e993aa6794aec48c6e39097c74f64a49b2cf
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-24 11:11:08 +0530

    1/100

--- Strings/love.java
+++ Strings/love.java
@@ -2,12 +2,12 @@ class love{
     public static void main(String[] args) {
         boolean ans=false;
         for (int i=0;i<=100;i++){
-            System.out.println("I love you Vinaya!!");
+            System.out.println("I love Java!!");
         }
         if (ans== true){
-            System.out.println("Please marry me!!");
+            System.out.println("I love java!!");
         }else{
-            System.out.println("Marry deepan!! ");
+            System.out.println("I dont love any language!! ");
         }       
      }
 }
