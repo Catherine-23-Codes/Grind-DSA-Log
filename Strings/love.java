3f5773944d2e0f5f1f92562e6c449a9319d83355
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-24 11:07:19 +0530

    1/100

---
+++ Strings/love.java
@@ -0,0 +1,13 @@
+class love{
+    public static void main(String[] args) {
+        boolean ans=false;
+        for (int i=0;i<=100;i++){
+            System.out.println("I love you Vinaya!!");
+        }
+        if (ans== true){
+            System.out.println("Please marry me!!");
+        }else{
+            System.out.println("Marry deepan!! ");
+        }       
+     }
+}
