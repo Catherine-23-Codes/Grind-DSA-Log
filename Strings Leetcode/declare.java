32bfbbe78956e3a1cb56771584a240a7eb2d5d8c
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-29 10:20:14 +0530

    6/100

---
+++ Strings Leetcode/declare.java
@@ -0,0 +1,13 @@
+public class declare {
+    public static void main(String[] args){
+// Method 1
+    // without using new keyowrd
+        String s1="Catherine";
+        System.out.println("the name is:"+s1);
+
+//Method 2
+    // with using new keyword
+        String s2=new String("Vinaya");
+        System.out.println("the name is:"+s2);
+    }
+}
