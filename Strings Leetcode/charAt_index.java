945ac0cc305c523f5c0d956436b3ce88d8ba1ae4
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-29 15:15:44 +0530

    6/100

---
+++ Strings Leetcode/charAt_index.java
@@ -0,0 +1,10 @@
+public class charAt_index {
+    public static void printIndexPos(){
+        String name="Catherine Infanta";
+        System.out.println(name.length());
+        System.out.println(name.charAt(7));
+    }
+    public static void main(String[] args) {
+        printIndexPos();
+    }
+}
