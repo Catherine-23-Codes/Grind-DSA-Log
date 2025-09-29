9853d86f96a119d9f60863c058c29cbaa2c5064d
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-29 11:04:18 +0530

    6/100

---
+++ Strings Leetcode/length_inbuilt.java
@@ -0,0 +1,10 @@
+public class length_inbuilt {
+    public static void main(String[] args) {
+        String str1="Catherine Infanta";
+        System.out.println("Length of the string is:"+str1.length());
+        // op is 17 as space is also counted
+
+        String s2="  C a t h e r i n e  ";
+        System.out.println("the length of the string is:"+s2.length());
+    }
+}
