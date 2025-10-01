f4e1040681e1dcc41ae4efbf65c16efa0efd6f3f
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-10-01 16:31:39 +0530

    1/10

---
+++ Strings Leetcode/Concatenation1.java
@@ -0,0 +1,8 @@
+public class Concatenation1 {
+    public static void main(String[] args) {
+        String s1="hello";
+        String s2="Catherine";
+        String s3=s1+ " "+ s2;
+        System.out.println(s3);
+    }
+}
