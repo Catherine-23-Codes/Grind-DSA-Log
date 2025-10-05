0c616ae89c626a4dacc6eb79c78fbe803549aacb
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-10-05 17:05:59 +0530

    5/10

---
+++ Strings Leetcode/ValidParanthesis.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+public class ValidParanthesis {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String test1 = "()";
+        String test2 = "([{}])";
+        String test3 = "(]";
+        String test4 = "([)]";
+
+        System.out.println(isValid(test1)); // true
+        System.out.println(isValid(test2)); // true
+        System.out.println(isValid(test3)); // false
+        System.out.println(isValid(test4)); // false
+    }
+}
