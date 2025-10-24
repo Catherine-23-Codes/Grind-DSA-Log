99a9c7785eaeccbb2f614b6ddbb097e884155fdc
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-10-24 11:22:58 +0530

    24/10/25

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n){
+        List<String> result=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                result.add("FizzBuzz");
+            } else if(i%3==0){
+                result.add("Fizz");
+            }else if(i%5==0){
+                result.add("Buzz");
+            }else{
+                result.add(Integer.toString(i));
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n=6;
+        FizzBuzz fb=new FizzBuzz();
+        List<String> result=fb.fizzBuzz(n);
+        for(String s:result){
+            System.out.println(s);
+        }
+    }
+}
