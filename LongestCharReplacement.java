93fdf206c95c885747d0f4d4929cf309b6f9af54
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-10-25 08:45:34 +0530

    25/10/2025

---
+++ LongestCharReplacement.java
@@ -0,0 +1,29 @@
+public class LongestCharReplacement {
+    public static int characterReplacement(String s, int k) {
+        int n = s.length(), maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            int maxFreq = 0;
+
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'A']++;
+                maxFreq = Math.max(maxFreq, freq[s.charAt(j) - 'A']);
+
+                int len = j - i + 1;
+                int changes = len - maxFreq;
+
+                if (changes <= k)
+                    maxLen = Math.max(maxLen, len);
+                else
+                    break;  // no need to expand further for this i
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("AABABBA", 1)); // 4
+        System.out.println(characterReplacement("PXQXYPIY", 2));    // 4
+    }
+}
